a3d5253193bd766bbb77f8ccc05f55a713b2d324
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-29 00:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ Handlebars.registerHelper('comment_body', function() {
     return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
   }
 })
+<<<<<<< HEAD
+=======
+
+function loadIssues() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+>>>>>>> 7ba40fb91d28d00cc9828e0748625a2f318847bc
 
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
